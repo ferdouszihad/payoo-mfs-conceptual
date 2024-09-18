0eb63737baa58a8885d2139a8c19ffe3e5b9fa91
ferdouszihad <f.zihad333@gmail.com>
2024-09-19 02:10:06 +0600

    video 4 : recap common function and integrate to cashout

--- js/cashOut.js
+++ js/cashOut.js
@@ -2,15 +2,18 @@ getElement("btn-cash-out").addEventListener("click", function (event) {
   event.preventDefault();
   console.log("cash out button clicked");
 
-  const cashOut = document.getElementById("input-cash-out").value;
-  const cashOutNumber = parseFloat(cashOut);
-  const pinNumber = document.getElementById("input-cash-out-pin").value;
+  //   const cashOut = document.getElementById("input-cash-out").value;
+  //   const cashOutNumber = parseFloat(cashOut);
+  //   const pinNumber = document.getElementById("input-cash-out-pin").value;
+  //   console.log(cashOut, pinNumber);
+  const cashOutNumber = getInputNumber("input-cash-out");
+  const pinNumber = getInputString("input-cash-out-pin");
 
-  console.log(cashOut, pinNumber);
   // wrong way to verify pin number
   if (pinNumber === "1234") {
-    const balance = document.getElementById("account-balance").innerText;
-    const balanceNumber = parseFloat(balance);
+    // const balance = document.getElementById("account-balance").innerText;
+    // const balanceNumber = parseFloat(balance);
+    const balanceNumber = getElementNumber("account-balance");
 
     // reduce the balance
     const newBalance = balanceNumber - cashOutNumber;
